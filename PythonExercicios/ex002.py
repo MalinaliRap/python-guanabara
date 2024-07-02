f0bafbde9bab72d6c99feae70d03c4a41b4cde93
Gabriel Becher <gabriel.becher@matriz.baston.com>
2024-07-02 10:02:42 -0300

    entrada formatada do Python 3

--- PythonExercicios/ex002.py
+++ PythonExercicios/ex002.py
@@ -1,4 +1,4 @@
 #exercicio para inserir seu nome e imprimir uma mensagem de boa vinda
 name = input('Digite seu nome: ')
 
-print('É um prazer te conhecer,', name)
+print('É um prazer te conhecer, {} !'.format(name))
