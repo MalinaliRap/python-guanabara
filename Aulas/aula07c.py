4904c1b0ebab26dfc9752feda5523518069a4022
Gabriel Coplas Becher <utfprdiario@gmail.com>
2024-07-02 21:37:50 -0300

    aula07cpy

---
+++ Aulas/aula07c.py
@@ -0,0 +1,7 @@
+nome = input('Qual é o seu nome? ')
+print('Prazer em te conhecer {:=^20}!'.format(nome))
+#{:>20} alinhado a direita
+#{:<20} alinhado a esquerda
+#{:^20} centralizado
+#{:.2f} arredondamento de casas decimais
+#{:=^20} centralizado e preenchendo o espaço com "="
