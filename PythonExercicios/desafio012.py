16c53bb545e810f294a15d16c76e05ed59182b2b
Gabriel Becher <gabriel.becher@matriz.baston.com>
2024-07-04 14:13:32 -0300

    melhorada no desafio 012

--- PythonExercicios/desafio012.py
+++ PythonExercicios/desafio012.py
@@ -1,4 +1,4 @@
 #faça um algoritmo que leia o preço de um produto e mostre seu novo preço, com 5% de desconto
 p = float(input('digite o valor do produto: '))
 d = p * 0.95
-print('o valor do produto com 5% de desconto e {:.2f}'.format(d))
+print('o valor do produto com 5% de desconto é R${:.2f}'.format(d))
