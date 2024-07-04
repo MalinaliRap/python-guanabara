06042048810166b1a9e518706aa2002fd93a228e
Gabriel Becher <gabriel.becher@matriz.baston.com>
2024-07-04 15:41:10 -0300

    exemplo de import melhorado

--- Aulas/aula08a.py
+++ Aulas/aula08a.py
@@ -1,4 +1,4 @@
 import math
 num = int(input('digite um valor: '))
 raiz = math.sqrt(num)
-print('a raiz quadrada de {} vale {}'.format(num, raiz))
+print('a raiz quadrada de {} vale {:.2f}'.format(num, raiz))
