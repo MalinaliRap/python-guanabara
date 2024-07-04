fad2c0fa72afe261ad1d196e3d622271cf26eb54
Gabriel Becher <gabriel.becher@matriz.baston.com>
2024-07-04 11:36:37 -0300

    melhorando desafio 008

--- PythonExercicios/desafio008.py
+++ PythonExercicios/desafio008.py
@@ -1,4 +1,25 @@
 #escreva um programa que leia um valor em metros e o exiba convertido em centimetros e milimitros.
 # 1 m = 100 cm = 1000 mm
 m = float(input('digite um valor em metros: '))
-print('O valor {} em metros equivale a {} cm e {} mm'.format(m, m*100, m*1000))
+
+# turbinada no exercicio após explicação
+#quilômetro → km
+#hectômetro → hm
+#decâmetro → dam
+#metro → m
+#decímetro → dm
+#centímetro → cm
+#milímetro → mm
+
+km = m / 1000
+hm = m / 100
+dam = m / 10
+dm = m * 10
+cm = m * 100
+mm = m * 1000
+
+print('o valor {} metros em milimetros é {} mm'.format(m, mm))
+print('o valor {} metros em centimetros é {} cm'.format(m, cm))
+print('o valor {} metros em decimetros é {} dm'.format(m, dm))
+print('o valor {} metros em decametros é {} dam'.format(m, dam))
+print('o valor {} metros em hectometros é {} hm'.format(m, hm))
