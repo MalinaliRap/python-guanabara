2c14f365bf976d5b22c4c49464d1b8b9a30151a0
Gabriel Coplas Becher <utfprdiario@gmail.com>
2024-07-04 21:25:31 -0300

    desafio 021

---
+++ Exercicios/desafio021.py
@@ -0,0 +1,8 @@
+#Faça um programa em Python que abra e reproduza o aúdio de um arquivo mp3
+
+from playsound import playsound
+
+playsound('C:/Users/gabri/Downloads/mp3.mp3')
+
+#tive dificuldade pois precisa instalar duas libs antes de playsound:
+#https://pypi.org/project/wheel/ e https://pypi.org/project/setuptools/
