1ef11afcf4b0a8bf6126cfaf07cdb7844fb0f257
Gabriel Becher <gabriel.becher@matriz.baston.com>
2024-07-12 14:00:54 -0300

    finalizando modulo 01

---
+++ Aulas/aula010extra.py
@@ -0,0 +1,28 @@
+# style : 
+# 0 - none
+# 1 - bold
+# 3 - underline
+# 4 - negative
+# 
+# cores fontes : 
+# 30 - branco
+# 31 - vermelho
+# 32 - verde
+# 33 - amarelo
+# 34 - azul
+# 35 - roxo
+# 36 - ciano
+# 37 - cinza
+#
+# cores fundo : 
+# 30 - branco
+# 31 - vermelho
+# 32 - verde
+# 33 - amarelo
+# 34 - azul
+# 35 - roxo
+# 36 - ciano
+# 37 - cinza
+#
+#exemplo \033[0:33:44m]
+print('\033[1;31;43mOlá, mundo!\033[m')
--- Exercicios/desafio028.py
+++ Exercicios/desafio028.py
@@ -1,11 +1,17 @@
 #Escreva um programa que faça o computador "pensar" em um número inteiro entre 0 e 5 e peça para o usuárioa tentar descobrir
 #qual foi o número escolhido pelo computador.
 #O programa deverá escrever na tela se o usuário venceu ou perdeu
+from time import sleep
 from random import randrange
 random_number = randrange(0,5)
-user_number = int(input('Digite um número de 1 a 5 : '))
+print('\033[0;30;45m-=-\033[m'*20)
+print('\033[1;37;41mVou pensar um número de 0 a 5, tente adivinhar ....\033[m')
+print('\033[0;30;45m-=-\033[m'*20)
+user_number = int(input('\033[4;33mDigite um número de 1 a 5 : \033[m'))
+print('\033[0;35mPROCESSANDO ...\033[m')
+sleep(3)
 if random_number == user_number:
-    print('Você ganhou!',end='')
+    print('\033[1;32mVocê ganhou!',end='')
 else:
-    print('Você perdeu!',end='')
-print(', o número era {}'.format(random_number))    
+    print('\033[1;31mVocê perdeu!',end='')
+print(', o número era {}\033[m'.format(random_number))    
