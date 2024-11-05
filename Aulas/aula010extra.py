7426eba5703f07b5cfc4b062ba2791b9c8565c93
Gabriel Becher <gabriel.becher@matriz.baston.com>
2024-11-05 09:25:27 -0300

    nem lembro

--- Aulas/aula010extra.py
+++ Aulas/aula010extra.py
@@ -15,14 +15,14 @@
 # 37 - cinza
 #
 # cores fundo : 
-# 30 - branco
-# 31 - vermelho
-# 32 - verde
-# 33 - amarelo
-# 34 - azul
-# 35 - roxo
-# 36 - ciano
-# 37 - cinza
+# 40 - branco
+# 41 - vermelho
+# 42 - verde
+# 43 - amarelo
+# 44 - azul
+# 45 - roxo
+# 46 - ciano
+# 47 - cinza
 #
 #exemplo \033[0:33:44m]
 print('\033[1;31;43mOlá, mundo!\033[m')
